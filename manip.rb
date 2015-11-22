86b4fe2924ec43d0c81f1b584462e9450f3c902e
bocal3 <pascaldengis@gmail.com>
2015-11-22 13:56:10 +0100

    Update manip.rb

--- manip.rb
+++ manip.rb
@@ -1,1 +1,28 @@
+class Utilisateur
+  attr_accessor :prenom, :ami
+  
+  def est_ami_avec?(prenom)
+    amis.each do |ami|
+      if ami.prenom == prenom
+        return true 
+      end
+    end
+    return false
+  end
+end
+
+alice = Utilisateur.new
+alice.prenom = "Alice"
+
+bob = Utilisateur.new
+bob.prenom = "Bob"
+
+jane = Utilisateur.new
+jane.prenom = "Jane"
+
+alice.amis = [bob, jane]
+
+puts alice.amis.size
+
+puts alice.est_ami_avec?("Bob")
 
