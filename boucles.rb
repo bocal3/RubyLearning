0c12c5e3179019a77d15131c0ce14d7e8ed18595
bocal3 <pascaldengis@gmail.com>
2015-11-22 13:02:40 +0100

    Update boucles.rb

--- boucles.rb
+++ boucles.rb
@@ -1,1 +1,10 @@
-
+utilisateurs = [
+  "Alice", "Bob", "John", "Hector"
+  ]
+  
+  // faire une boucle dans ce tableau en ruby
+  i = 0
+  utilisateurs.each do |utilisateur|
+    puts i = i + 1
+    puts utilisateur
+  end
