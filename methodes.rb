5b2d6879cb1c24c69ff6dc302cb023d1ae8164ea
bocal3 <pascaldengis@gmail.com>
2015-11-22 13:42:20 +0100

    Update methodes.rb

--- methodes.rb
+++ methodes.rb
@@ -1,1 +1,13 @@
+class Utilisateur
+  attr_accessor :prenom, :nom
+  
+  def ToString
+    prenom + " " + nom
+  end
+end
 
+mon_utilisateur = Utilisateur.new
+mon_utilisateur.prenom = "Bob"
+mon_utilisateur.nom = "Lenon"
+
+puts mon_utilisateur.ToString
