da03eb753f1219e38600c62bc00aac5d8c902f11
bocal3 <pascaldengis@gmail.com>
2015-11-22 13:28:04 +0100

    commentaire avec bonne syntaxe

--- boucles.rb
+++ boucles.rb
@@ -1,18 +1,18 @@
-// Les boucles
+# Les boucles
 utilisateurs = [
   "Alice", "Bob", "John", "Hector"
   ]
   
-  // faire une boucle dans ce tableau en ruby
+  # faire une boucle dans ce tableau en ruby
   i = 0
   utilisateurs.each do |utilisateur|
     puts i = i + 1
     puts utilisateur
   end
 
-// Les boucles de répétition
+# Les boucles de répétition
 10.times do |i|
-  // fait 10 fois
+  # fait 10 fois
   puts "Hello #{i}"
-  // #{i} placer l'indice en plus
+  # #{i} placer l'indice en plus
 end
