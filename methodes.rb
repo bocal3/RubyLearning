dfc0dd71f22fb1072735c4e5ae27658fb149f066
bocal3 <pascaldengis@gmail.com>
2015-11-22 13:46:29 +0100

    Avec des paramètres

--- methodes.rb
+++ methodes.rb
@@ -1,13 +1,22 @@
 class Utilisateur
-  attr_accessor :prenom, :nom
+  attr_accessor :prenom, :nom, :pays
   
   def ToString
     prenom + " " + nom
+  end
+  
+  def habite_en(valeur)
+    valeur == pays
   end
 end
 
 mon_utilisateur = Utilisateur.new
 mon_utilisateur.prenom = "Bob"
 mon_utilisateur.nom = "Lenon"
+mon_utilisateur.pays = "Belgique"
 
 puts mon_utilisateur.ToString
+
+# avec des paramètres
+puts mon_utilisateur.habite_en("Belgique")
+# rendra VRAI
