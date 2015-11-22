d705cea4c50dbef86360c018f643469eab0bb4df
bocal3 <pascaldengis@gmail.com>
2015-11-22 14:16:34 +0100

    Update composition.rb

--- composition.rb
+++ composition.rb
@@ -1,5 +1,11 @@
 class Eleve
-  attr_accessor :nom
+  attr_accessor :nom, :examens
+  def moyenne
+    total = 0
+    examens.each do |examen|
+      total += examen.note
+    end
+    total /examens.size
 end
 
 class Examen
@@ -14,5 +20,10 @@ end
 mon_eleve = Eleve.new
 mon_eleve.nom = "Billy"
 
-maths = Examen.new("maths", 10)
-philo = Examen.new("philo", 15)
+maths = Examen.new("maths", 10.0)
+philo = Examen.new("philo", 15.0)
+
+mon_eleve.eamen = [maths, philo]
+mon_eleve.examens.each do |examen|
+  puts examen.nom
+end
