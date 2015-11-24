ae6d18055e9a3f884cf438f0afe540107e3bb61d
bocal3 <pascaldengis@gmail.com>
2015-11-24 10:59:20 +0100

    Update bissextile.rb

--- bissextile.rb
+++ bissextile.rb
@@ -1,1 +1,9 @@
 
+puts "Entrez une annee : "
+annee = gets.chomp()
+annee = annee.to_i()
+if annee % 400 == 0 or (annee % 4 == 0 && annee % 100 != 0)
+   puts "L'annee est bissextile."
+else
+  puts "L'annee n'est pas bissextile."
+end
